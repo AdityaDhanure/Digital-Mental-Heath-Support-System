5ab834f15f663c83c5c06626e1f8ab47c45b9ce3
AdityaDhanure <adityadhanure0607@gmail.com>
2026-05-05 22:43:03 +0530

    5th commit

--- frontend-next/src/components/common/index.ts
+++ frontend-next/src/components/common/index.ts
@@ -2,5 +2,4 @@ export { Button } from './Button';
 export { Card } from './Card';
 export { Input } from './Input';
 export { Loading } from './Loading';
-export { Alert } from './Alert';
-export { Modal } from './Modal';
+
